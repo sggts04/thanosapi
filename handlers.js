fb1e8e91667610304df05ef9b7fec01e6fe885f1
Shreyas Gupta <technology.shreyas@gmail.com>
2019-03-30 14:19:48 +0530

    Initial Commit

---
+++ handlers.js
@@ -0,0 +1,32 @@
+const quotes = require('./quotes/quotes.json')
+
+function randomQuote() {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+function randomFiveQuotes() {
+    var indexes = [];
+    var data = [];
+    while (indexes.length < 5) {
+        let index = Math.floor(Math.random() * quotes.length);
+        if (indexes.includes(index)) {
+            continue;
+        } else {
+            indexes.push(index);
+        }
+    }
+    for (let index of indexes) {
+        data.push(quotes[index]);
+    }
+    return data;
+}
+
+function allQuotes() {
+    return quotes;
+}
+
+module.exports = {
+    randomQuote,
+    allQuotes,
+    randomFiveQuotes
+};
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const rateLimit = require("express-rate-limit");
+const {
+      randomQuote,
+      allQuotes,
+      randomFiveQuotes
+} = require('./handlers');
+const app = express();
+
+const limiter = rateLimit({
+      windowMs: 10 * 60 * 1000, // 10 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+      message: "Too many requests in too small time. Chill. Take a walk or something idk."
+});
+
+app.enable("trust proxy");
+app.set('view engine', 'ejs');
+app.set('json spaces', 4);
+app.use(limiter);
+
+app.get('/', (req, res) => {
+      res.render('index')
+})
+
+app.get('/random', (req, res) => {
+      res.json(randomQuote())
+})
+
+app.get('/random/5', (req, res) => {
+      res.json(randomFiveQuotes())
+})
+
+app.get('/all', (req, res) => {
+      res.json(allQuotes())
+})
+
+// Preparing for Heroku
+port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
